ff315e9055edafd7b84864b7bc08c12d26cbbe88
Tara Sherman <taradelin@gmail.com>
2019-10-26 11:52:08 -0400

    Created store Set up middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import './styles/index.css';
 import App from "./components/App";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  composeWithDevTools(
+    applyMiddleware(
+      thunk,
+      logger
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION__() :
+    f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
